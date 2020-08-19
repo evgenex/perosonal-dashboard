938ea40715db8c61c885e35a0049ffca4b2fb6c9
evgenex <evgenex@gmail.com>
2020-08-19 18:15:08 +0100

    build

---
+++ build/precache-manifest.61927efbe939067d85adf4dde87450ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9c3181b31b86fe99cdca32f057aea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de89a1064d219606b90b",
+    "url": "/static/css/main.ab835280.chunk.css"
+  },
+  {
+    "revision": "5c32c0a5f1189ae32b9b",
+    "url": "/static/js/2.98198ad9.chunk.js"
+  },
+  {
+    "revision": "a103d256531a2acb87553cdf3060ca05",
+    "url": "/static/js/2.98198ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de89a1064d219606b90b",
+    "url": "/static/js/main.42482200.chunk.js"
+  },
+  {
+    "revision": "db7cbd95383dea09d659",
+    "url": "/static/js/runtime-main.b8ac8752.js"
+  },
+  {
+    "revision": "131eee19c7301c8e594ac6ebab506d93",
+    "url": "/static/media/Background.131eee19.png"
+  }
+]);
